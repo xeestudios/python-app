be7ab7a1700be82fa49cc7829a502fc8aa1ccdca
Zeeshan Eqbal <zeeshan@1click2deliver.com>
2021-03-18 01:03:09 +0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>API Interface</h1>
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios';
+class App extends Component {
+  componentDidMount() {
+    const api = 'https://t7agkz0332.execute-api.us-east-2.amazonaws.com/beta/transactions?transactionId=44&type=PAYLOAD&amount=22';
+    const data = { "transactionId" : "33", "type" : "Test", "amount" : "100" };
+    axios
+      .post(api, data, {
+        headers: {
+          'Authorization': `x-api-key B1eKIV3HbeNjglcpVjEu3gMa0BZYCXs4XcvpnMI5`
+        },
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  render() {
+    return (
+      <div>Medium Tutorial</div>
+    );
+  }
 }
-
 export default App;
