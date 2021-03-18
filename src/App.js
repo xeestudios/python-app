84a7287d790e6ba62129d82113bf909f82432db2
Zeeshan Eqbal <zeeshan@1click2deliver.com>
2021-03-18 19:51:45 +0400

    Update m2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ class App extends Component {
     const api = 'https://t7agkz0332.execute-api.us-east-2.amazonaws.com/beta/transactions?transactionId=44&type=PAYLOAD&amount=22';
     const data = { "transactionId" : "33", "type" : "Test", "amount" : "100" };
     axios
-      .post(api, data)
+      .post(api, data, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length"
+        },
+      })
       .then((response) => {
         console.log(response);
       })
