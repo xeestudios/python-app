059d979930415d1b3e879d8d83c2419c38fd596e
Zeeshan Eqbal <zeeshan@1click2deliver.com>
2021-03-19 20:41:15 +0400

    asd

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import React, { Component } from 'react';
 import axios from 'axios';
 class App extends Component {
   componentDidMount() {
-    const api = 'https://5bsos88fy6.execute-api.us-east-2.amazonaws.com/prod/resource';
+    //const api = 'https://5bsos88fy6.execute-api.us-east-2.amazonaws.com/prod/resource';
+    const api = 'https://5iczn48u19.execute-api.us-east-2.amazonaws.com/prod/myPython';
     const data = { "transactionId" : "33", "type" : "Test", "amount" : "100" };
     axios
-      .post(api, data, {
-        headers: {
-          "Accept" : "*/*",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, x-amz-security-token, x-amz-date, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length"
-        },
-      })
+      .post(api, data)
       .then((response) => {
         console.log(response);
       })
