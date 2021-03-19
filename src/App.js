de13eb76456cea07b656caaa6da0b73da1b30ace
Zeeshan Eqbal <zeeshan@1click2deliver.com>
2021-03-19 20:59:17 +0400

    asdasd

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 class App extends Component {
   componentDidMount() {
-    //const api = 'https://5bsos88fy6.execute-api.us-east-2.amazonaws.com/prod/resource';
-    const api = 'https://5iczn48u19.execute-api.us-east-2.amazonaws.com/prod/myPython';
+    const api = 'https://5bsos88fy6.execute-api.us-east-2.amazonaws.com/prod/resource';
+    //const api = 'https://5iczn48u19.execute-api.us-east-2.amazonaws.com/prod/myPython';
     const data = { "transactionId" : "33", "type" : "Test", "amount" : "100" };
     axios
       .post(api, data)
