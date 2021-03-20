47b4c11695586d592d39e3d9ed94d31f0f164565
Zeeshan Eqbal <zeeshan@1click2deliver.com>
2021-03-20 12:09:12 +0400

    adasd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {person: []};
+    
+  }
+
+
   componentDidMount() {
-    const api = 'https://5bsos88fy6.execute-api.us-east-2.amazonaws.com/prod/resource';
-    //const api = 'https://5iczn48u19.execute-api.us-east-2.amazonaws.com/prod/myPython';
-    const data = { "transactionId" : "33", "type" : "Test", "amount" : "100" };
+    //const api = 'https://5bsos88fy6.execute-api.us-east-2.amazonaws.com/prod/resource';
+    const api = 'https://5iczn48u19.execute-api.us-east-2.amazonaws.com/prod/myPython';
+    const data = { "first_name" : "Ahmed", "last_name" : "Rabi"};
     axios
       .post(api, data)
       .then((response) => {
         console.log(response);
+        this.setState({ person: response.data.message })
       })
       .catch((error) => {
         console.log(error);
       });
+      
     }
   render() {
+    const persons = this.state.person;
     return (
-      <div>Medium Tutorial</div>
+      <div>{ persons }</div>
     );
   }
 }
